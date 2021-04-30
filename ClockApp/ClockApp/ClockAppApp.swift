90b4c432da7489a32941d49f1381db8e8bd75299
ahmadbarizie17 <83440580+ahmadbarizie17@users.noreply.github.com>
2021-04-30 14:50:41 +0700

    Add files via upload

---
+++ ClockApp/ClockApp/ClockAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClockAppApp.swift
+//  ClockApp
+//
+//  Created by AHMAD BARIZIE SIROJUL ILMI on 10/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ClockAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ClockApp/ClockApp/ContentView.swift
@@ -0,0 +1,170 @@
+//
+//  ContentView.swift
+//  ClockApp
+//
+//  Created by AHMAD BARIZIE SIROJUL ILMI on 10/11/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State var isDark = false
+    var body: some View {
+            
+        NavigationView{
+            
+            Home(isDark: $isDark)
+                .navigationBarHidden(true)
+                .preferredColorScheme(isDark ? .dark : .light)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct Home : View {
+    
+    @Binding var isDark : Bool
+    var width = UIScreen.main.bounds.width
+    @State var curreant_Time = Time(min: 0, sec: 0, hour: 0)
+    @State var reciever = Timer.publish(every: 1, on: .current, in: .default).autoconnect()
+    
+    var body: some View{
+        
+        VStack{
+            
+            HStack{
+                
+                Text("JAM !!!!!")
+                    .font(.title)
+                    .fontWeight(.heavy)
+                
+                Spacer(minLength: 0)
+                
+                Button(action: {isDark.toggle()}) {
+                    
+                    Image(systemName: isDark ? "sun.min.fill" : "moon.fill")
+                        .font(.system(size: 22))
+                        .foregroundColor(isDark ? .black : .white)
+                        .padding()
+                        .background(Color.primary)
+                        .clipShape(Circle())
+                    
+                }
+            }
+            .padding()
+            
+            Spacer(minLength: 0)
+            
+            ZStack{
+                
+                Circle()
+                    .fill(Color("Color").opacity(0.1))
+                
+                //second and min dots....
+                
+                ForEach(0..<60,id: \.self){i in
+                    
+                    Rectangle()
+                        .fill(Color.primary)
+                        //60/12=5
+                        .frame(width: 2, height: (i % 5) == 0 ? 15 : 5)
+                        .offset(y: (width - 110) / 2)
+                        .rotationEffect(.init(degrees: Double(i) * 6))
+                
+                }
+                
+                //Sec...
+                
+                Rectangle()
+                    .fill(Color.primary)
+                    .frame(width: 2, height: (width - 180) / 2)
+                    .offset(y: -(width - 180) / 4)
+                    .rotationEffect(.init(degrees: Double(curreant_Time.sec) * 6))
+                
+                //min...
+                
+                Rectangle()
+                    .fill(Color.primary)
+                    .frame(width: 4, height: (width - 200) / 2)
+                    .offset(y: -(width - 200) / 4)
+                    .rotationEffect(.init(degrees: Double(curreant_Time.min) * 6))
+                
+                //hrs...
+                
+                Rectangle()
+                    .fill(Color.primary)
+                    .frame(width: 4.5, height: (width - 240) / 2)
+                    .offset(y: -(width - 240) / 4)
+                    .rotationEffect(.init(degrees: Double(curreant_Time.hour + (curreant_Time.min / 60)) * 30))
+                
+                //center circle
+                
+                Circle()
+                    .fill(Color.primary)
+                    .frame(width: 15, height: 15)
+            }
+            .frame(width: width - 80, height: width - 80)
+            
+            //getting locale region name...
+            
+            Text(Locale.current.localizedString(forRegionCode: Locale.current.regionCode!) ?? "")
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .padding(.top,35)
+            
+            Text(getTime())
+                .font(.system(size: 45))
+                .fontWeight(.heavy)
+                .padding(.top,10)
+            
+            Spacer(minLength: 0)
+        }
+        .onAppear(perform: {
+            
+            let calendar = Calendar.current
+        
+            let min = calendar.component(.minute, from: Date())
+            let sec = calendar.component(.second, from: Date())
+            let hour = calendar.component(.hour, from: Date())
+        
+            withAnimation(Animation.linear(duration: 0.01)){
+            
+                self.curreant_Time = Time(min: min, sec: sec, hour: hour)
+            }
+        })
+        .onReceive(reciever) { (_) in
+            
+            let calendar = Calendar.current
+        
+            let min = calendar.component(.minute, from: Date())
+            let sec = calendar.component(.second, from: Date())
+            let hour = calendar.component(.hour, from: Date())
+        
+            withAnimation(Animation.linear(duration: 0.01)){
+            
+                self.curreant_Time = Time(min: min, sec: sec, hour: hour)
+            }
+        }
+    }
+    
+    func getTime()->String{
+        
+        let format = DateFormatter()
+        format.dateFormat = "hh:mm: a"
+        
+        return format.string(from: Date())
+    }
+}
+
+//calculating time....
+
+struct Time {
+    var min : Int
+    var sec : Int
+    var hour : Int
+}
